7223f8ad3d8001f9e9282512348915771c2db3a9
Rishi Desai <desai.rishi1@gmail.com>
2021-06-08 20:15:15 -0700

    c hello world, initial nix setup

---
+++ c/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A calculator written in C.";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.abacus-c =
+      let pkgs = import nixpkgs {
+            system = "x86_64-linux";
+          };
+      in pkgs.stdenv.mkDerivation {
+        pname = "abacus-c";
+        version = "1.0.0";
+        src = "./abacus.c";
+
+        buildPhase = ''
+                   gcc abacus.c -o abacus-c
+        '';
+
+        installPhase = ''
+                     mkdir $out/bin
+                     cp abacus-c $out/bin/.
+        '';
+      };
+
+    # Specify the default package
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.abacus-c;
+  };
+}
