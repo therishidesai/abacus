f723020054ef2459bac145c47502620418429308
Rishi Desai <desai.rishi1@gmail.com>
2021-06-14 23:18:10 -0700

    Basic string parsing:  - can read integers using strtol  - can read ops  - skip all unknown chars

--- c/src/abacus.c
+++ c/src/abacus.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "test.h"
 #define BUFFER_SIZE 1024
 
+
+// TODO: return operator type
+int is_operator(char op) {
+	int ret_val = 0;
+	switch(op) {
+
+	case '(' :
+		ret_val = 6;
+		break;
+	case ')' :
+		ret_val = 5;
+		break;
+	case '*' :
+		ret_val = 4;
+		break;
+	case '/' :
+		ret_val = 3;
+			break;
+	case '+' :
+		ret_val = 2;
+			break;
+	case '-' :
+		ret_val = 1;
+			break;
+	default :
+		ret_val = 0;
+	}
+
+	return ret_val;
+}
+
+// TODO: Have this return a data structure of the parsed input.
+void parse_input(const char *input) {
+	long int num;
+	while (input != NULL && *input != '\0') {
+		if (isdigit(*input)) {
+			num = strtol(input, &input, 10);
+			printf("%ld\n", num);
+		} else if (is_operator(*input)) {
+			printf("%c\n", *input);
+			++input;
+		} else {
+			++input;
+		}
+	}
+}
 
 int main() {
 	int exit_flag = 0;
@@ -20,6 +67,8 @@ int main() {
 			if (strcmp(input, "exit\n") == 0) {
 				printf("BYE!\n");
 				exit_flag = 1;
+			} else {
+				parse_input(input);
 			}
 		} else {
 			printf("BYE!\n");
